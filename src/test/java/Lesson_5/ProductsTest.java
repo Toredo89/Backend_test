3751fadf970065319c5a55b18a1b593158418329
Toredo <toredo89@mail.ru>
2022-06-07 11:28:02 +0500

    Выполнил дз-5

--- src/test/java/Lesson_5/ProductsTest.java
+++ src/test/java/Lesson_5/ProductsTest.java
@@ -1,6 +1,8 @@
 package Lesson_5;
 
+import Lesson_5.api.CategoryService;
 import Lesson_5.api.ProductService;
+import Lesson_5.dto.GetCategoryResponse;
 import Lesson_5.dto.Product;
 import Lesson_5.utils.RetrofitUtils;
 import com.github.javafaker.Faker;
@@ -18,37 +20,21 @@ import java.io.IOException;
 
 import static jdk.javadoc.internal.tool.Main.execute;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
-public class ProductsTest {
-    static ProductService productService;
-    Product product = null;
-    Faker faker = new Faker();
-    int id;
-
+public class ProductsTest<GetProductsResponse> {
+    static CategoryService categoryService;
     @BeforeAll
     static void beforeAll() {
-        productService = RetrofitUtils.getRetrofit()
-                .create(ProductService.class);
-    }
-
-    @BeforeEach
-    void setUp() {
-        product = new Product()
-                .withTitle(faker.food().ingredient())
-                .withCategoryTitle("Food")
-                .withPrice((int) (Math.random() * 20000));
-    }
-
-    @Test
-    void getProductsTest() throws IOException {
-        Response<ResponseBody> response = productService.getProducts(product).execute();
-        assertThat(response.isSuccessful(), CoreMatchers.is(false));
+        categoryService = RetrofitUtils.getRetrofit().create(CategoryService.class);
     }
 
     @SneakyThrows
-    @AfterEach
-    void tearDown() {
-        Response<ResponseBody> response = productService.deleteProduct(id).execute();
-        assertThat(response.isSuccessful(), CoreMatchers.is(false));
+    @Test
+    void getProductsTest() {
+        Response<GetProductsResponse> response = (Response<GetProductsResponse>) categoryService.getProducts().execute();
+
+        assertThat(response.isSuccessful(), CoreMatchers.is(true));
     }
+
 }
